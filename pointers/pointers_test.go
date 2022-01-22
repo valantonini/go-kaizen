6f2b52d8848b828e117a8a54d70e3c20fea77230
val antonini <val.antonini@protonmail.com>
2022-01-22 11:48:47 +1000

    code cleanup

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -2,25 +2,24 @@ package pointers
 
 import (
 	"github.com/matryer/is"
-	"github.com/valantonini/go-kaizen/assert"
 	"testing"
 )
 
 func Test_Pointers(t *testing.T) {
-	is := is.New(t)
+	Is := is.New(t)
 	t.Run("pointer addresses", func(t *testing.T) {
-		var x int = 7
+		var x = 7
 		var p *int      // *int == pointer to int
 		p = &x          // &x == address of x
-		is.Equal(*p, 7) // *p == value at address
+		Is.Equal(*p, 7) // *p == value at address
 	})
 
 	t.Run("value at pointer address", func(t *testing.T) {
 		x := 1
 		p := &x // p, of type *int, points to x
-		assert.Equal(t, *p, 1)
+		Is.Equal(*p, 1)
 
 		*p = 2 // equivalent to x = 2
-		assert.Equal(t, x, 2)
+		Is.Equal(x, 2)
 	})
 }
