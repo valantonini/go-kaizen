841c28b1ec156a81aa40bd54ee632c9593450885
val antonini <val.antonini@protonmail.com>
2022-02-09 17:56:44 +1000

    how pointers are passed by value to functions parameters

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -7,6 +7,7 @@ import (
 
 func Test_Pointers(t *testing.T) {
 	Is := is.New(t)
+
 	t.Run("pointer addresses", func(t *testing.T) {
 		var x = 7
 		var p *int      // *int == pointer to int
@@ -21,5 +22,23 @@ func Test_Pointers(t *testing.T) {
 
 		*p = 2 // equivalent to x = 2
 		Is.Equal(x, 2)
+	})
+
+	t.Run("pointer parameters are passed by value and do not reassign", func(t *testing.T) {
+		target := 777
+		originalTargetAddress := &target
+
+		var parameterAddress *int
+		reassignPointer := func(p *int) {
+			parameterAddress = p
+			localNum := 888
+			// the address is passed by value to the function. We are reassigning the parameter's address and not the
+			// original target's address. If we wanted to reassign the target, we'd need to pass in a **int
+			p = &localNum
+		}
+
+		reassignPointer(&target)
+		Is.True(parameterAddress == originalTargetAddress)
+		Is.True(&target == originalTargetAddress)
 	})
 }
