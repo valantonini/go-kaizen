2856477b7fa2172d4307ca60e2f79c42fe1907b8
val antonini <val.antonini@protonmail.com>
2022-02-12 12:01:59 +1000

    functional-options additional options and checks

--- functional-options/function_options_test.go
+++ functional-options/function_options_test.go
@@ -28,4 +28,31 @@ func Test_FunctionalOptions(t *testing.T) {
 		Is.NoErr(err)
 		Is.Equal(server.Port, 80)
 	})
+
+	t.Run("it sets the port", func(t *testing.T) {
+		server, err := NewServer("", WithPort(8080))
+
+		Is.NoErr(err)
+		Is.Equal(server.Port, 8080)
+	})
+
+	t.Run("it errors if the port is below 0", func(t *testing.T) {
+		_, err := NewServer("", WithPort(-1))
+
+		Is.Equal(err, PortLessThanZeroError)
+	})
+
+	t.Run("it defaults the scheme to http when not supplied", func(t *testing.T) {
+		server, err := NewServer("")
+
+		Is.NoErr(err)
+		Is.Equal(server.Scheme, "http")
+	})
+
+	t.Run("it sets the scheme when not supplied", func(t *testing.T) {
+		server, err := NewServer("", WithScheme("https"))
+
+		Is.NoErr(err)
+		Is.Equal(server.Scheme, "https")
+	})
 }
--- functional-options/functional_options.go
+++ functional-options/functional_options.go
@@ -1,20 +1,65 @@
 package functional_options
 
+import "errors"
+
 type options struct {
-	port *int
+	port   *int
+	scheme string
 }
 
 type Option func(o *options) error
 
 type Server struct {
-	Host string
-	Port int
+	Host   string
+	Port   int
+	Scheme string
 }
 
-func NewServer(host string) (*Server, error) {
+var PortLessThanZeroError = errors.New("port must be 0 or greater")
+
+func WithPort(p int) Option {
+	return func(o *options) error {
+		if p < 0 {
+			return PortLessThanZeroError
+		}
+		o.port = &p
+		return nil
+	}
+}
+
+func WithScheme(s string) Option {
+	return func(o *options) error {
+		o.scheme = s
+		return nil
+	}
+}
+
+// NewServer - “2.11.3 Functional options pattern”
+// 100 Go Mistakes and How to Avoid Them MEAP V09
+func NewServer(host string, opts ...Option) (*Server, error) {
 	if host == "" {
 		host = "localhost"
 	}
+	var options options
 
-	return &Server{host, 80}, nil
+	for _, o := range opts {
+		err := o(&options)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var port int
+	if options.port == nil {
+		port = 80
+		options.port = &port
+	}
+
+	if options.scheme == "" {
+		options.scheme = "http"
+	}
+
+	server := Server{Host: host, Port: *options.port, Scheme: options.scheme}
+
+	return &server, nil
 }
