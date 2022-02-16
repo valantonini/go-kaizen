f3901f9e20b09bb31a18874903ef2fb0549481f0
val antonini <val.antonini@protonmail.com>
2022-02-17 06:49:48 +1000

    additional experimenting with reflection

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -50,7 +50,7 @@ func Test_Reflection(t *testing.T) {
 		Is.True(r.Field(0).Kind() == reflect.String)
 	})
 
-	t.Run("using reflection to dereference a pointer", func(t *testing.T) {
+	t.Run("using reflection to dereference a pointer to a value", func(t *testing.T) {
 
 		occupation := "engineer"
 		person := struct {
@@ -65,5 +65,30 @@ func Test_Reflection(t *testing.T) {
 
 		Is.True(r.Field(1).Kind() == reflect.Ptr)
 		Is.Equal(r.Field(1).Elem().String(), "engineer")
+	})
+
+	t.Run("using reflection to dereference a pointer to a struct", func(t *testing.T) {
+		type occupation struct {
+			title string
+		}
+		person := struct {
+			Name       string
+			Occupation *occupation
+		}{
+			"arnold",
+			&occupation{"engineer"},
+		}
+
+		r := reflect.ValueOf(person)
+
+		Is.True(r.Field(1).Kind() == reflect.Ptr)
+
+		occupationField := r.Field(1).Elem()
+		Is.Equal(occupationField.Kind(), reflect.Struct)
+
+		// could be improved by using a recursive approach
+		// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reflection#refactor-6
+		occupationTitle := reflect.ValueOf(occupationField.Interface()).Field(0)
+		Is.Equal(occupationTitle.String(), "engineer")
 	})
 }
