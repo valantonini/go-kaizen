b97e3b41a0af243ee98ec26939b33b7a98396ade
val antonini <val.antonini@protonmail.com>
2022-03-15 19:53:57 +1000

    more learning around slices and backing array

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -21,4 +21,33 @@ func Test_Slices(t *testing.T) {
 		Is.Equal(subslice[0], 1)
 		Is.Equal(subslice[1], 2) //value from source[2] assignment above
 	})
+
+	t.Run("slices can be declared with a length and a capacity of the backing array", func(t *testing.T) {
+		source := make([]int, 2, 3)
+		source[0] = 0
+		source[1] = 1
+
+		// will panic, length is only 2 even though the backing array is 3
+		source = append(source, 2)
+
+		Is.Equal(len(source), 3)
+		Is.Equal(cap(source), 3)
+
+		subslice := source[0:3]
+		Is.Equal(subslice[0], 0)
+		Is.Equal(subslice[1], 1)
+		Is.Equal(subslice[2], 2)
+
+		// change first value of original slice
+		source[0] = 4
+		Is.Equal(subslice[0], 4)
+
+		// this will cause backing array to be reallocated (doubles size when capacity is reached)
+		subslice = append(subslice, 6)
+		Is.Equal(cap(subslice), 6)
+
+		// source and subslice now use 2 different backing arrays. this will only update the source
+		source[0] = 7
+		Is.Equal(subslice[0], 4)
+	})
 }
